a96e11fd54f0bfbf0a82808be8dc34e687eac383
sagarbisht07 <bishtsagarbisht1232gmail.com>
2023-05-11 17:56:57 +0530

    basic ts app with tailwind css

---
+++ my-app/src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-7xl text-blue-500"
+      
+      
+      >Hello world!</h1>
+    </div>
+  );
+}
+
+export default App;
