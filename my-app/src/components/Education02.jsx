db6ad6d6a5c0904e6ce046f5e4ca11a7776bb160
Neeraj Bisht <bishtnrj1418@gmail.com>
2023-05-14 10:54:29 +0530

    education

---
+++ my-app/src/components/Education02.jsx
@@ -0,0 +1,308 @@
+export const Education02 = () => {
+  return (
+    <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+      <div className="grid gap-5 lg:grid-cols-3 sm:max-w-sm sm:mx-auto lg:max-w-full">
+        <div className="overflow-hidden transition-shadow duration-300 bg-white rounded">
+          <a
+            href="https://www.runtastic.com/blog/en/muscle-gain-101/"
+            aria-label="Article"
+          >
+            <img
+              src="https://images.pexels.com/photos/3076509/pexels-photo-3076509.jpeg?auto=compress&cs=tinysrgb&w=1260"
+              className="object-cover w-full h-64 rounded"
+              alt=""
+            />
+          </a>
+          <div className="py-5">
+            <p className="mb-2 text-xs font-semibold text-gray-600 uppercase">
+              13 Jul 2020
+            </p>
+            <a
+              href="/"
+              aria-label="Article"
+              className="inline-block mb-3 text-black transition-colors duration-200 hover:text-deep-purple-accent-700"
+            >
+              <p className="text-2xl font-bold leading-5">Diving to the deep</p>
+            </a>
+            <p className="mb-4 text-teal-700">
+              Are you ready to unleash your inner athlete and take your fitness
+              journey to the next level? Then you've come to the right place.
+              Our blog is dedicated to providing you with the latest insights,
+              tips, and strategies for achieving your health and wellness goals.
+              Whether you're looking to lose weight, build muscle, improve your
+              flexibility, or simply feel better in your own skin, we've got you
+              covered.
+            </p>
+            <div className="flex space-x-4">
+              <a
+                href="/"
+                aria-label="Likes"
+                className="flex items-start text-gray-800 transition-colors duration-200 hover:text-deep-purple-accent-700 group"
+              >
+                <div className="mr-2">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    className="w-5 h-5 text-gray-600 transition-colors duration-200 group-hover:text-deep-purple-accent-700"
+                  >
+                    <polyline
+                      points="6 23 1 23 1 12 6 12"
+                      fill="none"
+                      strokeMiterlimit="10"
+                    />
+                    <path
+                      d="M6,12,9,1H9a3,3,0,0,1,3,3v6h7.5a3,3,0,0,1,2.965,3.456l-1.077,7A3,3,0,0,1,18.426,23H6Z"
+                      fill="none"
+                      stroke="currentColor"
+                      strokeMiterlimit="10"
+                    />
+                  </svg>
+                </div>
+                <p className="font-semibold">7.4K</p>
+              </a>
+              <a
+                href="/"
+                aria-label="Comments"
+                className="flex items-start text-gray-800 transition-colors duration-200 hover:text-deep-purple-accent-700 group"
+              >
+                <div className="mr-2">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    stroke="currentColor"
+                    className="w-5 h-5 text-gray-600 transition-colors duration-200 group-hover:text-deep-purple-accent-700"
+                  >
+                    <polyline
+                      points="23 5 23 18 19 18 19 22 13 18 12 18"
+                      fill="none"
+                      strokeMiterlimit="10"
+                    />
+                    <polygon
+                      points="19 2 1 2 1 14 5 14 5 19 12 14 19 14 19 2"
+                      fill="none"
+                      stroke="currentColor"
+                      strokeMiterlimit="10"
+                    />
+                  </svg>
+                </div>
+                <p className="font-semibold">81</p>
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="overflow-hidden transition-shadow duration-300 bg-white rounded">
+          <a
+            href="https://www.runtastic.com/blog/en/eating-less-to-lose-weight/"
+            aria-label="Article"
+          >
+            <img
+              src="https://img.freepik.com/free-vector/child-obesity-overweight-abstract-concept-vector-illustration-child-nutrition-unhealthy-habit-overweight-treatment-children-eating-disorder-kids-fast-food-addiction-abstract-metaphor_335657-5984.jpg?size=626&ext=jpg&ga=GA1.2.1225246930.1683882460&semt=ais"
+              className="object-cover w-full h-64 rounded"
+              alt=""
+            />
+          </a>
+          <div className="py-5">
+            <p className="mb-2 text-xs font-semibold text-gray-600 uppercase">
+              4 Nov 2020
+            </p>
+            <a
+              href="/"
+              aria-label="Article"
+              className="inline-block mb-3 text-black transition-colors duration-200 hover:text-deep-purple-accent-700"
+            >
+              <p className="text-2xl font-bold leading-5">Conquer the World</p>
+            </a>
+            <p className="mb-4 text-teal-700">
+              Another important aspect of fitness is variety. Doing the same
+              workout over and over again can quickly lead to boredom and
+              burnout. That's why it's important to mix things up and try new
+              things. Whether it's trying a new exercise class, exploring a new
+              hiking trail, or experimenting with different healthy recipes,
+              adding variety to your routine can help keep you engaged and
+              excited about your fitness journey.
+            </p>
+            <div className="flex space-x-4">
+              <a
+                href="/"
+                aria-label="Likes"
+                className="flex items-start text-gray-800 transition-colors duration-200 hover:text-deep-purple-accent-700 group"
+              >
+                <div className="mr-2">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    className="w-5 h-5 text-gray-600 transition-colors duration-200 group-hover:text-deep-purple-accent-700"
+                  >
+                    <polyline
+                      points="6 23 1 23 1 12 6 12"
+                      fill="none"
+                      strokeMiterlimit="10"
+                    />
+                    <path
+                      d="M6,12,9,1H9a3,3,0,0,1,3,3v6h7.5a3,3,0,0,1,2.965,3.456l-1.077,7A3,3,0,0,1,18.426,23H6Z"
+                      fill="none"
+                      stroke="currentColor"
+                      strokeMiterlimit="10"
+                    />
+                  </svg>
+                </div>
+                <p className="font-semibold">7.4K</p>
+              </a>
+              <a
+                href="/"
+                aria-label="Comments"
+                className="flex items-start text-gray-800 transition-colors duration-200 hover:text-deep-purple-accent-700 group"
+              >
+                <div className="mr-2">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    stroke="currentColor"
+                    className="w-5 h-5 text-gray-600 transition-colors duration-200 group-hover:text-deep-purple-accent-700"
+                  >
+                    <polyline
+                      points="23 5 23 18 19 18 19 22 13 18 12 18"
+                      fill="none"
+                      strokeMiterlimit="10"
+                    />
+                    <polygon
+                      points="19 2 1 2 1 14 5 14 5 19 12 14 19 14 19 2"
+                      fill="none"
+                      stroke="currentColor"
+                      strokeMiterlimit="10"
+                    />
+                  </svg>
+                </div>
+                <p className="font-semibold">81</p>
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="overflow-hidden transition-shadow duration-300 bg-white rounded">
+          <a
+            href="https://www.runtastic.com/blog/en/the-7-best-bodyweight-exercises-for-strong-triceps/"
+            aria-label="Article"
+          >
+            <img
+              src="https://images.pexels.com/photos/6787216/pexels-photo-6787216.jpeg?auto=compress&cs=tinysrgb&w=1260"
+              className="object-cover w-full h-64 rounded"
+              alt=""
+            />
+          </a>
+          <div className="py-5">
+            <p className="mb-2 text-xs font-semibold text-gray-600 uppercase">
+              28 Dec 2020
+            </p>
+            <a
+              href="/"
+              aria-label="Article"
+              className="inline-block mb-3 text-black transition-colors duration-200 hover:text-deep-purple-accent-700"
+            >
+              <p className="text-2xl font-bold leading-5">
+                Explore the beautiful
+              </p>
+            </a>
+            <p className="mb-4 text-teal-700">
+              Of course, one of the biggest challenges in fitness is staying
+              motivated. It's easy to start strong but much harder to maintain
+              that momentum over the long term. That's why it's important to
+              find your "why" and stay connected to your reasons for pursuing
+              fitness. Whether it's to improve your health, feel more confident,
+              or set a positive example for your kids, keeping your goals in
+              mind can help you push through the tough times and stay focused on
+              the prize.
+            </p>
+            <div className="flex space-x-4">
+              <a
+                href="/"
+                aria-label="Likes"
+                className="flex items-start text-gray-800 transition-colors duration-200 hover:text-deep-purple-accent-700 group"
+              >
+                <div className="mr-2">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    className="w-5 h-5 text-gray-600 transition-colors duration-200 group-hover:text-deep-purple-accent-700"
+                  >
+                    <polyline
+                      points="6 23 1 23 1 12 6 12"
+                      fill="none"
+                      strokeMiterlimit="10"
+                    />
+                    <path
+                      d="M6,12,9,1H9a3,3,0,0,1,3,3v6h7.5a3,3,0,0,1,2.965,3.456l-1.077,7A3,3,0,0,1,18.426,23H6Z"
+                      fill="none"
+                      stroke="currentColor"
+                      strokeMiterlimit="10"
+                    />
+                  </svg>
+                </div>
+                <p className="font-semibold">7.4K</p>
+              </a>
+              <a
+                href="/"
+                aria-label="Comments"
+                className="flex items-start text-gray-800 transition-colors duration-200 hover:text-deep-purple-accent-700 group"
+              >
+                <div className="mr-2">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    stroke="currentColor"
+                    className="w-5 h-5 text-gray-600 transition-colors duration-200 group-hover:text-deep-purple-accent-700"
+                  >
+                    <polyline
+                      points="23 5 23 18 19 18 19 22 13 18 12 18"
+                      fill="none"
+                      strokeMiterlimit="10"
+                    />
+                    <polygon
+                      points="19 2 1 2 1 14 5 14 5 19 12 14 19 14 19 2"
+                      fill="none"
+                      stroke="currentColor"
+                      strokeMiterlimit="10"
+                    />
+                  </svg>
+                </div>
+                <p className="font-semibold">81</p>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
--- my-app/src/pages/Educational.jsx
+++ my-app/src/pages/Educational.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import { Education01 } from "../components/Education01";
+import { Education02 } from "../components/Education02";
+import { Education03 } from "../components/Education03";
+import { Education04 } from "../components/Education04";
 
 const Educational = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Education01 />
+      <Education02 />
+      <Education03 />
+      <Education04 />
+    </div>
+  );
 };
 
 export default Educational;
